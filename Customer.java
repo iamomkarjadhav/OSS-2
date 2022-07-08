d94b4729518277b2448f33cdb065a24b7e729b15
OmkarSantoshChavan <108923290+OmkarSantoshChavan@users.noreply.github.com>
2022-07-08 16:20:54 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,21 @@
+
+public class Customer {
+	private String emailid;
+	private String name;
+	public Customer()
+	{
+		emailid="NA";
+		name="NA";
+	}
+	public Customer(String emailid,String name)
+	{
+		this.emailid=emailid;
+		this.name=name;
+	}
+	public void display()
+	{
+		System.out.println("Emmail id: "+emailid);
+		System.out.println("Name     : "+name);
+	}
+
+}
